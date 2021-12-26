5b4f15ebd509598bb26a49563be5541d65caa2ce
Ben Naylor <36206067+bnaylor1989@users.noreply.github.com>
2021-12-26 17:19:00 +0000

    Added solc version

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,16 @@
+module.exports = {};
+
 module.exports = {
   networks: {
     local: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
-    }
-  }
+      network_id: "*", // Match any network id
+    },
+  },
+  // compilers: {
+  //   solc: {
+  //     version: "0.8.7",
+  //   },
+  // },
 };
